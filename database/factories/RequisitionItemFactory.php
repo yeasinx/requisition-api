135db85b27059e8603dd75f836ef78e7f403c1e0
yeasinx <yeasin.code@gmail.com>
2026-08-31 14:46:56 +0600

    chore: add requisition database factories and seeders

---
+++ database/factories/RequisitionItemFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\RequisitionItem;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<RequisitionItem>
+ */
+class RequisitionItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
