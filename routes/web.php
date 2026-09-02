78f281251a1f91660c2f88f6fa8fbb7e056e159a
yeasinx <yeasin.code@gmail.com>
2026-09-02 12:59:57 +0600

    feat: return json health check from root route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => config('app.name'),
+        'status' => 'OK',
+        'version' => '1.0.0',
+    ]);
 });
